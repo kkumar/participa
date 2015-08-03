d3bb938df39fdca26304a84a49b014353549c7c5
Andrés Pereira <apereira@alabs.es>
2015-08-03 23:54:47 +0200

    [deploy] unicorn staging zec

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,9 +1,10 @@
 directory = "/var/www/beta.participa.zaragozaencomun.com"
+socket = "/tmp/unicorn.beta.participa.zaragozaencomun.com.sock"
 
 working_directory "#{directory}/current"
 pid "#{directory}/current/tmp/pids/unicorn.pid"
 stderr_path "#{directory}/shared/log/unicorn.log"
 stdout_path "#{directory}/shared/log/unicorn.log"
-listen "/tmp/unicorn.betaparticipa.barcelonaencomu.cat.sock"
+listen socket
 worker_processes 4
 timeout 120
