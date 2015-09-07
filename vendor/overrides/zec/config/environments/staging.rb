091f2e0eecb44be93f2e5a67353b56c66c038613
Andrés Pereira <apereira@alabs.es>
2015-09-07 13:02:03 -0500

    [deploy] adding amazon ses for sending mails

--- vendor/overrides/zec/config/environments/staging.rb
+++ vendor/overrides/zec/config/environments/staging.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = [:es]
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :ses
 
   #ActionMailer::Base.delivery_method = :smtp
   #ActionMailer::Base.smtp_settings = {
