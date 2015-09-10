ea16042a7b432bfea7286110c34f42eb443f2215
Hilfe <daniel.hilfe@gmail.com>
2015-09-10 13:27:30 +0200

    Adding send mail method

--- vendor/overrides/zec/config/environments/production.rb
+++ vendor/overrides/zec/config/environments/production.rb
@@ -3,5 +3,5 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = [:es]
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :ses
 end
